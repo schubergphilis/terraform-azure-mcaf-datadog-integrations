5006fa30c9eb3928a0197d894c58cdae6b8d04fb
Bogdan Grozoiu <bogdan@grozoiu.com>
2025-03-12 16:50:11 +0100

    fix: correct claims mapping policy ID reference in assignment

--- msft-entraid.tf
+++ msft-entraid.tf
@@ -131,7 +131,7 @@ resource "azuread_claims_mapping_policy" "datadog_saml_auth_claims_mapping_polic
 }
 
 resource "azuread_service_principal_claims_mapping_policy_assignment" "app" {
-  claims_mapping_policy_id = one(azuread_claims_mapping_policy.datadog_saml_auth_claims_mapping_policy).id
+  claims_mapping_policy_id = azuread_claims_mapping_policy.datadog_saml_auth_claims_mapping_policy.id
   service_principal_id     = azuread_service_principal.datadog_saml_auth_enterprise_application.id
 }
 
