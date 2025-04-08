00f47098f346637fee055ed606d610044600add7
Marius Waldhorst <mwaldhorst@schubergphilis.com>
2025-04-08 10:48:37 +0200

    fix: Fix version lower bound of datadog provider

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     datadog = {
       source  = "datadog/datadog"
-      version = "~> 3.0.0"
+      version = "~> 3.59"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
