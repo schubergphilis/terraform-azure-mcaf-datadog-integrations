26b566c08d01ac27c2237959eb8555f9d604e69b
Marius Waldhorst <mwaldhorst@schubergphilis.com>
2025-04-08 10:05:16 +0200

    fix: Add missing required provider. Update version constraints to be less open. Bump minimum terraform version.

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,21 @@
 terraform {
-  required_version = ">= 1.8"
+  required_version = ">= 1.9"
   required_providers {
     datadog = {
       source  = "datadog/datadog"
-      version = ">= 3.0.0"
+      version = "~> 3.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4"
+      version = "~>= 4"
     }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 2.53.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">=0.13"
     }
   }
 }
