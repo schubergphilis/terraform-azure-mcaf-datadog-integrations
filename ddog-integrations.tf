708bb8a9638ae6780220cd4f8900122e0fbeb811
Sander de Vries <sdevries@schubergphilis.com>
2025-05-13 16:16:40 +0200

    terraform fmt, remove whitespace

--- ddog-integrations.tf
+++ ddog-integrations.tf
@@ -41,7 +41,7 @@ resource "datadog_integration_slack_channel" "slack_integration_monitoring" {
 
 resource "datadog_integration_opsgenie_service_object" "opsgenie_integration" {
   for_each = { for idx, opsgenie_integration in var.opsgenie_integration : idx => opsgenie_integration }
-     
+
   name             = each.value.name
   opsgenie_api_key = data.azurerm_key_vault_secret.opsgenie_api_key[each.key].value
   region           = each.value.region
